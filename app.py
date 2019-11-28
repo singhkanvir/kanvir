4c988aade78fd7668ccba38068b24ef9d04f3760
singhkanvir <58293235+singhkanvir@users.noreply.github.com>
2019-11-28 19:28:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def upload():
 
 
 if __name__ == '__main__':
-    # app.run(port=5002, debug=True)
+     app.run(port=8000, debug=True)
 
     # Serve the app with gevent
     http_server = WSGIServer((), app)
